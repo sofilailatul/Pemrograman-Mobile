32b9f5f8e6428f5c3760779e9ef1c7393c67b540
sofilailatul <lailatulsofi11@gmail.com>
2024-09-12 10:38:21 +0700

    first commit

---
+++ codelab2/codelab2/bin/codelab2.dart
@@ -0,0 +1,7 @@
+import 'package:codelab2/codelab2.dart' as codelab2;
+
+void main() {
+  for (int i = 18; i > 8; i--) {
+    print('Nama saya adalah Sofi, sekarang berumur $i');
+  }
+}
---
+++ codelab2/codelab2/test/codelab2_test.dart
@@ -0,0 +1,8 @@
+import 'package:codelab2/codelab2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
